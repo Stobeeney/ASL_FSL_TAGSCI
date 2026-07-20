ef879223929b1080006d2d6dad325ebd22e0bfaa
Rein Cosare <reincosare07@gmail.com>
2026-07-20 23:06:13 +0800

    Trigger deployment with new author email reincosare07@gmail.com

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import numpy as np
 from flask import Flask, render_template, jsonify, request, send_file
 from dotenv import load_dotenv
 
+# Trigger deploy with updated author account
 load_dotenv()
 
 app = Flask(__name__)
